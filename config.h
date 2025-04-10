a9c9f4a1a9eb7adc8e231e55e2b218bc2e7b20d3
kanjibro <142528531+kanjibro@users.noreply.github.com>
2025-04-10 10:59:08 +0545

    Create config.h

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi Credentials
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+// Spotify API Credentials
+#define SPOTIFY_CLIENT_ID "your_client_id"
+#define SPOTIFY_CLIENT_SECRET "your_client_secret"
+#define SPOTIFY_REFRESH_TOKEN "your_refresh_token"
+
+#endif
